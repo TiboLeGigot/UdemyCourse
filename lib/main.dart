db8a2488689f0b50162e94e3c322e34f66fbc1cf
Thibault Jeannequin <t.jeannequin@gmail.com>
2020-07-20 16:04:32 +0200

    [Chapitre 42]

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  var questions = [
+    "name ?",
+    "age ?",
+  ];
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('yo'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('What\'s your favorite food ?'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex],
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text("1"),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('2'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
